295f97bca71d273595c033b898fb2128dcea172c
Tim Dill <brodt7258@gmail.com>
2018-09-03 17:56:59 -0400

    configured tests to talk to mongo server

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 let browser, page;
 
@@ -29,24 +31,10 @@ test('clicking login starts Oauth flow', async () => {
 });
 
 test('when signed in, shows logout button', async () => {
-	const id = '5b859517765e504b5014e87f';
+	const user = await userFactory();
+	const { session, sig } = sessionFactory(user);
 
-	const Buffer = require('safe-buffer').Buffer;
-	const sessionObject = {
-		passport: {
-			user: id
-		}
-	};
-	const sessionString = Buffer.from(
-		JSON.stringify(sessionObject)
-	).toString('base64');
-
-	const Keygrip = require('keygrip');
-	const keys = require('../config/keys');
-	const keygrip = new Keygrip([keys.cookieKey]);
-	const sig = keygrip.sign('session=' + sessionString);
-
-	await page.setCookie({ name: 'session', value: sessionString });
+	await page.setCookie({ name: 'session', value: session });
 	await page.setCookie({ name: 'session.sig', value: sig });
 	await page.goto('localhost:3000');
 	await page.waitFor('a[href="/auth/logout"]');
